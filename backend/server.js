fe0c2b870ada804895b39330083db76f65bd6d15
pankajbinwal001 <pankajbinwal38@gmail.com>
2024-10-18 10:25:22 +0530

    deployment main

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const path = require('path')
 dotenv.config();
 const cookieParser = require('cookie-parser')
 
@@ -9,17 +10,26 @@ const messageRoutes = require('./routes/messageRoutes')
 const userRoutes = require('./routes/userRoutes')
 const conversationRoutes = require('./routes/conversationRoutes');
 const { app, server } = require('./socket/socket');
+
 app.use(express.json());
 app.use(cookieParser());
 
-// app.get('/', (req, res) => {
-//     res.send('API is running !')
-// })
+
 
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+
+
+
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     connectToMongoDB();
